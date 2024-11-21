a5e5e8872bed975bf557434424b12f6afac429bf
Barbz <guzmanrb@myumanitoba.ca>
2024-11-20 22:21:07 -0600

    testing stuff, commiting before I go to sleep

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { VStack, Heading, Text, Button, Center, AbsoluteCenter } from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit fgdfg<code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <VStack>
+        <Center>
+          <Heading>Purohit Nurse Practitioner Clinic</Heading>
+        </Center>
+        <AbsoluteCenter>
+          <Text fontSize="xl">Phone: 2043901861 Fax: 204...</Text>
+        </AbsoluteCenter>
+      </VStack>
     </div>
   );
 }
